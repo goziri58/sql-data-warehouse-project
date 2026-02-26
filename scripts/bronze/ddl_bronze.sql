5213ac59410a1a40f40214fbaf35c688786d1f00
goziri58 <goziri58@gmail.com>
2026-02-25 19:31:57 -0500

    Refactor bronze schema DDL and remove load procedure
    Removed the stored procedure for loading the bronze layer and added DDL for multiple bronze tables.

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -1,39 +1,103 @@
-/*
-======================================================================================
-Stored Procedure: Load Bronze Layer (Source -> Bronze)
-======================================================================================
-Script Purpose:
-    This stored procedure loads data into the 'bronze' schema from external CSV files.
-    It Performs the following actions:
-    - Truncates the bronze tables before loading data.
-    - Uses th 'BULK INSERT' command to load data from CSV Files to bronze tables.
 
-Parameters:
-    None.
-  This stored procedure does not accept any parameters or return any values.
 
-Usage Example:
-    EXEC bronze.load_bronze;
-======================================================================================
+/*creating ddl for bronze schema
+
+
 */
 
+CREATE TABLE bronze.crm_cust_info (
+	cst_id int4 NULL,
+	cst_key varchar(50) NULL,
+	cst_firstname varchar(50) NULL,
+	cst_lastname varchar(50) NULL,
+	cst_marital_status varchar(50) NULL,
+	cst_gndr varchar(50) NULL,
+	cst_create_date date NULL
+);
 
-CREATE OR REPLACE PROCEDURE bronze.load_bronze()
- LANGUAGE plpgsql
-AS $procedure$
-	BEGIN
-		begin 
-			raise notice '===============================================';
-			raise notice 'Loading bronze layer';
-			raise notice '===============================================';
-	
-			raise notice '-----------------------------------------------';
-			raise notice 'Loading CRM Table';
-			raise notice '-----------------------------------------------';
-	
 
--- Bulk loading the data for (bronze.crm)
-	
+
+
+
+CREATE TABLE bronze.crm_prd_info (
+	prd_id int4 NULL,
+	prd_key varchar(50) NULL,
+	prd_name varchar(50) NULL,
+	prd_cost int4 NULL,
+	prd_line varchar(50) NULL,
+	prd_start_dt date NULL,
+	prd_end_dt date NULL
+);
+
+
+
+
+CREATE TABLE bronze.crm_sales_details (
+	sls_ord_num varchar(50) NULL,
+	sls_prd_key varchar(50) NULL,
+	sls_cust_id int4 NULL,
+	sls_order_dt text NULL,
+	sls_ship_dt date NULL,
+	sls_due_dt date NULL,
+	sls_sales int4 NULL,
+	sls_quantity int4 NULL,
+	sls_price text NULL
+);
+
+
+
+
+CREATE TABLE bronze.erp_cust_az12 (
+	cid varchar(50) NULL,
+	bdate date NULL,
+	gen varchar(50) NULL
+);
+
+
+
+
+CREATE TABLE bronze.erp_loc_a101 (
+	cid varchar(50) NULL,
+	cntry varchar(50) NULL
+);
+
+
+
+
+
+CREATE TABLE bronze.erp_px_cat_g1v2 (
+	id varchar(50) NULL,
+	cat varchar(50) NULL,
+	subcat varchar(50) NULL,
+	maintenance varchar(50) NULL
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	
 	
 			raise notice '>> Truncating Table: bronze.crm_cust_info';
